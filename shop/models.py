56b3a16b9668fb94c3d59f52d426d129ec22fe4f
guillaume71 <guillaume271293@hotmail.fr>
2023-02-23 12:57:05 +0100

    API

---
+++ shop/models.py
@@ -0,0 +1,65 @@
+from django.conf import settings
+from django.db import models
+
+
+class Project(models.Model):
+    BACKEND = 'BACKEND'
+    FRONTEND = 'FRONTEND'
+    IOS = 'IOS'
+    ANDROID = 'ANDROID'
+    TYPES_CHOICES = (
+        (BACKEND, 'Back-end'),
+        (FRONTEND, 'Front-end'),
+        (IOS, 'iOS'),
+        (ANDROID, 'Android')
+    )
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    type = models.CharField(max_length=30)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='projects')
+
+    def __str__(self):
+        return self.title
+
+
+class Contributor(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name='contributors')
+    project = models.ForeignKey('Project',
+                                on_delete=models.CASCADE,
+                                related_name='contributors')
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='issues_auteur')
+    assignee_user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                                      on_delete=models.CASCADE,
+                                      related_name='issues_assigne')
+    priority = models.CharField(max_length=50)
+    tag = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    created_time = models.DateTimeField(auto_now_add=True)
+    project = models.ForeignKey('Project',
+                                on_delete=models.CASCADE,
+                                related_name='issues')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    description = models.TextField()
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
+    issue = models.ForeignKey('Issue',
+                              on_delete=models.CASCADE,
+                              related_name='comments')
+    created_time = models.DateTimeField(auto_now_add=True)
