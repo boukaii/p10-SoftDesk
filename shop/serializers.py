ba66b9e835c48dfb97a25e576ae23acbbeb73869
guillaume71 <guillaume271293@hotmail.fr>
2023-02-23 12:50:14 +0100

    API

---
+++ shop/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework.serializers import ModelSerializer
+from shop.models import Contributor, Issue, Project, Comment
+from django.contrib.auth.models import User
+from rest_framework import serializers
+import django.contrib.auth.password_validation as validators
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id',
+                  'username',
+                  'first_name',
+                  'last_name',
+                  'email',
+                  'password']
+
+    # create_user to have the hash
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    # password not check with create_user, need the following code
+    @staticmethod
+    def validate_password(data):
+        validators.validate_password(password=data, user=User)
+        return
+
+
+class ProjectSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = [
+            "id",
+            "title",
+            "description",
+            "type",
+            "author_user_id",
+        ]
+
+
+class ContributorSerializer(ModelSerializer):
+    class Meta:
+        model = Contributor
+        fields = ["id", "user_id", "project_id"]
+
+
+class IssueSerializer(ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = [
+            "id",
+            "title",
+            "description",
+            "tag",
+            "priority",
+            "status",
+            "project_id",
+            "author_user_id",
+            "assignee_user",
+            "created_time",
+        ]
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "created_time", "description", "author_user_id", "issue_id"]
